3964c60bd2b9ca237bd15e4a0afcf3329342034e
Shane Madden <shanemadden@fake.email>
2018-11-25 19:54:48 -0700

    Add migration for unlocking loaders when the mod's installed in existing games

---
+++ migrations/logistics-recipes.lua
@@ -0,0 +1,12 @@
+for _, force in pairs(game.forces) do
+	force.reset_technologies()
+	for tech_name, technology in pairs(force.technologies) do
+		if technology.researched then
+			for _, effect in pairs(technology.effects) do
+				if effect.type == "unlock-recipe" then
+					force.recipes[effect.recipe].enabled = true
+				end
+			end
+		end
+	end
+end
