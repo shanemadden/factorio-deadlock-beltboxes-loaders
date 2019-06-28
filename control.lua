4568ebb9aebf893c176d3ba0c0dd082db49eb74e
meonester <meonester@gmail.com>
2019-06-28 17:26:08 +0200

    Fixed loaders snapping.
    Fixed loaders snapping when building loaders.
    Added snapping when building next to loaders.

--- control.lua
+++ control.lua
@@ -26,6 +26,12 @@ local opposite = {
 	[defines.direction.east] = defines.direction.west,
 	[defines.direction.west] = defines.direction.east,
 }
+
+local opposite_type = {
+	["input"] = "output",
+	["output"] = "input",
+}
+
 local dir2vector = {
 	[defines.direction.north] = {x=0, y=-1},
 	[defines.direction.south] = {x=0, y=1},
@@ -38,10 +44,29 @@ local function add_vectors(v1, v2)
 	return {v1.x + v2.x, v1.y + v2.y}
 end
 
+-- check if it is neighbour of entity in given direction 
+local function get_the_neighbour(entity, neighbour, direction)
+	local entities = entity.surface.find_entities_filtered{type = neighbour.type,  position = add_vectors(entity.position, dir2vector[direction])}
+	for _, item in pairs(entities) do
+		if item == neighbour then return {neighbour} end
+	end
+	return {}
+end
+
 -- return all entities 1 tile away in specified direction
 local function get_neighbour_entities(entity, direction)
 	return entity.surface.find_entities_filtered{ position = add_vectors(entity.position, dir2vector[direction]) }
 end
+
+local function get_neighbour_loaders_for_entity(entity)
+	local position = entity.position
+	local box = entity.prototype.selection_box
+	local area = {
+	  {position.x + box.left_top.x-1, position.y + box.left_top.y-1},
+	  {position.x + box.right_bottom.x + 1, position.y + box.right_bottom.y + 1}
+	}
+	return entity.surface.find_entities_filtered{type="loader", area=area, force=entity.force}
+  end
 
 -- does any entity in list have an inventory we can work with
 local function are_loadable(entities)
@@ -61,7 +86,8 @@ local function are_belt_facing(entities, direction)
 	for _,entity in pairs(entities) do
 		if (entity.type == "transport-belt" or
 			entity.type == "underground-belt" or
-			entity.type == "splitter") and
+			entity.type == "splitter" or
+			entity.type == "loader") and
 			entity.direction == direction
 		then return true end
 	end
@@ -76,46 +102,91 @@ end
 local function on_built_entity(event)
 	local built = event.created_entity
 	-- invalid build or fake player build from pseudo-bot mods?
-	if not built or not built.valid or event.revived or built.type ~= "loader" or not created_loader[built.name] then
-		return
-	end
-	local snap2inv = settings.get_player_settings(game.players[event.player_index])["deadlock-loaders-snap-to-inventories"].value
-	local snap2belt = settings.get_player_settings(game.players[event.player_index])["deadlock-loaders-snap-to-belts"].value
-	-- no need to check anything if configs are off
-	if not snap2inv and not snap2belt then
-		return
-	end
-	-- get the entities from both ends
-	local belt_end = get_neighbour_entities(built, built.direction)
-	local loading_end = get_neighbour_entities(built, opposite[built.direction])
-	
-	if snap2belt and are_belt_facing(belt_end, opposite[built.direction]) then
-		-- there's a belt facing toward the belt-side of the loader, so we want to be in input mode
-		built.rotate( {by_player = event.player_index} )
-	elseif snap2belt and are_belt_facing(belt_end, built.direction) then
-		-- there's a belt facing away from the belt-side of the loader, so we want to be certain to stay in output mode, stop further checks
-		return
-	elseif snap2inv and are_loadable(loading_end) then
-		-- there's a loadable entity on the loader end, flip into input mode to load it up
-		built.rotate( {by_player = event.player_index} )
-	elseif are_loadable(belt_end) then
-		-- there's a loadable entity on the belt end but not on the loader end, flip around and go into input mode to load it up
-		built.direction = opposite[built.direction]
-		-- unless there's a belt facing away, then stay in output mode
-		if not are_belt_facing(loading_end, built.direction) then
-			-- that wasn't the case so we're safe to go into input mode
-			built.rotate( {by_player = event.player_index} )
+
+	if built and built.valid and not event.revived and built.type == "loader" and created_loader[built.name] then
+		local snap2inv = settings.get_player_settings(game.players[event.player_index])["deadlock-loaders-snap-to-inventories"].value
+		local snap2belt = settings.get_player_settings(game.players[event.player_index])["deadlock-loaders-snap-to-belts"].value
+		-- no need to check anything if configs are off
+		if not snap2inv and not snap2belt then
+			return
 		end
-	elseif snap2belt and are_belt_facing(loading_end, built.direction) then
-		-- there's a belt facing into the loader end, switch into input mode and flip
-		built.direction = opposite[built.direction]
-		built.rotate( {by_player = event.player_index} )
-	elseif snap2belt and are_belt_facing(loading_end, opposite[built.direction]) then
-		-- there's a belt facing away from the loader end, flip
-		built.direction = opposite[built.direction]
+
+		local loader = built;
+		local belt_side_direction = loader.direction
+		-- if loader in input mode, switch
+		if loader.loader_type == "input" then belt_side_direction = opposite[loader.direction] end
+
+		local belt_end = get_neighbour_entities(loader, belt_side_direction)
+		local loading_end = get_neighbour_entities(loader, opposite[belt_side_direction])
+
+		-- note: loader mode won't change when snapped to inventory
+		if snap2belt and are_belt_facing(belt_end, loader.direction) then
+			-- belt on belt side, same direction
+		elseif snap2belt and are_belt_facing(belt_end, opposite[loader.direction]) then
+			-- belt on belt side, opposite direction
+			loader.rotate( {by_player = event.player_index} )
+		elseif snap2inv and are_loadable(loading_end) then
+			-- inventory on loading side
+		elseif snap2inv and are_loadable(belt_end) then
+			-- inventory on belt side
+			loader.direction = opposite[loader.direction]
+		elseif snap2belt and are_belt_facing(loading_end, loader.direction) then
+			-- belt on loading on belt side, same direction
+			loader.direction = opposite[loader.direction]
+			loader.rotate( {by_player = event.player_index} )
+		elseif snap2belt and are_belt_facing(loading_end, opposite[loader.direction]) then
+			-- belt on loading on belt side, opposite direction
+			loader.direction = opposite[loader.direction]
+		end
+		--loader.surface.create_entity{name="flying-text", position={loader.position.x-.25, loader.position.y-.5}, text = "^", color = {g=1}}
+	
+	elseif built and built.valid and not event.revived and built.type ~= "loader" then
+		-- todo: maybe filter only for belt-like and entities with inventories
+		
+		local snap2inv = settings.get_player_settings(game.players[event.player_index])["deadlock-loaders-snap-to-inventories"].value
+		local snap2belt = settings.get_player_settings(game.players[event.player_index])["deadlock-loaders-snap-to-belts"].value
+		if not snap2inv and not snap2belt then
+			return
+		end
+
+		local loaders = get_neighbour_loaders_for_entity(built)
+		for _, loader in pairs(loaders) do
+			if created_loader[loader.name] then
+
+				local belt_side_direction = loader.direction
+				-- if loader in input mode, switch
+				if loader.loader_type == "input" then belt_side_direction = opposite[loader.direction] end
+
+				local belt_end = get_the_neighbour(loader, built, belt_side_direction)
+				local loading_end = get_the_neighbour(loader, built, opposite[belt_side_direction])
+
+				-- note: loader mode won't change when snapped to inventory
+
+				if snap2belt and are_belt_facing(belt_end, loader.direction) then
+					-- belt on belt side, same direction
+				elseif snap2belt and are_belt_facing(belt_end, opposite[loader.direction]) then
+					-- belt on belt side, opposite direction
+					loader.rotate( {by_player = event.player_index} )
+				elseif snap2inv and are_loadable(loading_end) then
+					-- inventory on loading side
+				elseif snap2inv and are_loadable(belt_end) then
+					-- inventory on belt side
+					loader.direction = opposite[loader.direction]
+				elseif snap2belt and are_belt_facing(loading_end, loader.direction) then
+					-- belt on loading on belt side, same direction
+					loader.direction = opposite[loader.direction]
+					loader.rotate( {by_player = event.player_index} )
+				elseif snap2belt and are_belt_facing(loading_end, opposite[loader.direction]) then
+					-- belt on loading on belt side, opposite direction
+					loader.direction = opposite[loader.direction]
+				end
+				--loader.surface.create_entity{name="flying-text", position={loader.position.x-.25, loader.position.y-.5}, text = "*", color = {r=1}}
+			end
+		end	
 	end
 end
-script.on_event(defines.events.on_built_entity, on_built_entity)
+script.on_event({defines.events.on_built_entity}, on_built_entity)
+
 
 -- auto-unstacking by ownlyme
 local function on_picked_up_item(event)
